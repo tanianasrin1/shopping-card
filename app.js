328c3afc330a2849ac973c73c9ea78d2e0f2db38
tanianasrin1 <tasrineva169@gmail.com>
2022-02-14 01:11:55 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('case-plus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number')
+    const caseNumber = caseInput.value
+    caseInput.value = parseInt(caseNumber)+1
+    
+})
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number')
+    const caseNumber = caseInput.value
+    caseInput.value = parseInt(caseNumber)-1
+    
+})
+
