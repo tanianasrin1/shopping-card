8fc49f4948d4b61e4eec22e414f91e12a33cabec
tanianasrin1 <tasrineva169@gmail.com>
2022-02-14 01:47:38 +0600

    update

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number')
+    let caseNumber = caseInput.value
+    if(isIncreasing == true){
+        caseNumber = parseInt(caseNumber)+1
+    }
+    else if( caseNumber > 0){
+       caseNumber = parseInt(caseNumber)-1
+    }
+
+    caseInput.value = caseNumber;
+
+    const caseTotal = document.getElementById('case-total')
+    caseTotal.innerText = caseNumber*59;
+
+}
+
 document.getElementById('case-plus').addEventListener('click', function(){
-    const caseInput = document.getElementById('case-number')
-    const caseNumber = caseInput.value
-    caseInput.value = parseInt(caseNumber)+1
-    
+
+    updateCaseNumber(true)
+      
 })
+
 document.getElementById('case-minus').addEventListener('click', function(){
-    const caseInput = document.getElementById('case-number')
-    const caseNumber = caseInput.value
-    caseInput.value = parseInt(caseNumber)-1
+   
+    updateCaseNumber(false)
     
 })
 
